64b208f6ecf5df6bac6531f38cab734fbd240231
VelkaressiaBlutkrone <deepestdark@gmail.com>
2026-02-20 09:09:58 +0900

    inital commit

--- src/main/java/com/metacoding/springv2/core/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/metacoding/springv2/core/filter/JwtAuthorizationFilter.java
@@ -2,9 +2,14 @@ package com.metacoding.springv2.core.filter;
 
 import java.io.IOException;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.metacoding.springv2.core.util.JwtProvider;
+import com.metacoding.springv2.core.util.JwtUtil;
+import com.metacoding.springv2.user.User;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -27,6 +32,23 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+        // localhost:8080/api/good
+        // header -> Authorization : Bearer JWT토큰
+
+        String jwt = request.getHeader("Authorization");
+
+        if (jwt == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwt = jwt.replace("Bearer ", "");
+
+        User user = JwtUtil.verify(jwt);
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
         filterChain.doFilter(request, response);
     }
 }
